ac0c690b358c7c43c7b4939102101a7673748bdf
AyubParsaev-1981 <ayubparsaev1981@gmail.com>
2026-08-27 20:11:04 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from dotenv import load_dotenv
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# .env файлини юклаш
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    bot_token: str = "YOUR_BOT_TOKEN_HERE"
+    bot_token: str = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
 
     model_config = SettingsConfigDict(
         env_file=".env",
