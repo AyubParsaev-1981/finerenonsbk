8091f6f31a3596a7a83e7832c75c3596be782ed1
AyubParsaev-1981 <ayubparsaev1981@gmail.com>
2026-08-27 20:45:19 +0500

    Update/Add handlers/__init__.py with analytics support

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,12 @@
 from aiogram import Router
+from .admin import admin_router
 from .common import common_router
 from .full_calc import full_calc_router
 from .gfr_calc import gfr_calc_router
 from .acr_calc import acr_calc_router
 
 main_router = Router()
+main_router.include_router(admin_router)
 main_router.include_router(common_router)
 main_router.include_router(full_calc_router)
 main_router.include_router(gfr_calc_router)
