5d936dfeadae4fa1f25732fce4cf8f7730e5244a
AyubParsaev-1981 <ayubparsaev1981@gmail.com>
2026-08-27 19:47:40 +0500

    Add/Update states.py

---
+++ states.py
@@ -0,0 +1,35 @@
+"""
+Telegram Bot FSM (Finite State Machine) ҳолатлари.
+"""
+
+from aiogram.fsm.state import State, StatesGroup
+
+
+class FullCalcStates(StatesGroup):
+    """Тўлиқ СБК (КФТ + АКН + KDIGO) ҳисоблаш ҳолатлари."""
+    gender = State()
+    age = State()
+    weight = State()
+    blood_creatinine_unit = State()
+    blood_creatinine = State()
+    urine_albumin_unit = State()
+    urine_albumin = State()
+    urine_creatinine_unit = State()
+    urine_creatinine = State()
+
+
+class GFRCalcStates(StatesGroup):
+    """Фақат КФТ (Cockcroft-Gault ва CKD-EPI) ҳисоблаш ҳолатлари."""
+    gender = State()
+    age = State()
+    weight = State()
+    blood_creatinine_unit = State()
+    blood_creatinine = State()
+
+
+class ACRCalcStates(StatesGroup):
+    """Фақат Сийдик Альбумин/Креатинин нисбати (АКН) ҳисоблаш ҳолатлари."""
+    urine_albumin_unit = State()
+    urine_albumin = State()
+    urine_creatinine_unit = State()
+    urine_creatinine = State()
